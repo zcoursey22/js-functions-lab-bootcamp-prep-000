c6ee43f56e015bd6ea9dda9f070a77c2e6efb96c
Zachary Coursey <zcoursey22@gmail.com>
2018-01-30 05:37:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 // write your code below!
 function happyHolidays(name) {
+  return `Happy holidays!`
+}
+
+function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
 }
